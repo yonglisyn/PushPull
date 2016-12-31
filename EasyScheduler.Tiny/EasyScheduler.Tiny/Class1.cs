e9444cbd3782cd601856f019e58ba850ca0ceee4
yonglisyn <sylyongli@gmail.com>
2016-12-31 11:52:18 +0800

    EasyScheduler.Tiny

---
+++ EasyScheduler.Tiny/EasyScheduler.Tiny/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyScheduler.Tiny
+{
+    public class Class1
+    {
+    }
+}
